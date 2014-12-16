8830be53fa79e2d3b9d342d629122f4f9b820d6b
lotkeamol <amollotke@yahoo.co.in>
2014-12-16 22:12:51 +0000

    Domain Models Created

---
+++ Source/Lion.Domain/Models/CountryDetail.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lion.Domain.Models
+{
+    public class CountryDetail
+    {
+        [Key]
+        [ScaffoldColumn(false)]
+        public short CountryID { get; set; }
+
+        [StringLength(25)]
+        public string CountryName { get; set; }
+
+        [StringLength(5)]
+        public string CountryCode { get; set; }
+
+        [StringLength(5)]
+        public string ISDCode { get; set; }
+        
+        [ScaffoldColumn(false)]
+        public short CurrencyID { get; set; }
+
+        public virtual Currency Currency { get; set; }
+
+    }
+}
