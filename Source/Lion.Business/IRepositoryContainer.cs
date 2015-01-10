9b7ef32b1573fc14eb0b99184dcab25f058de1ef
lotkeamol <amollotke@yahoo.co.in>
2015-01-10 18:45:37 +0000

    Left overs

---
+++ Source/Lion.Business/IRepositoryContainer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleInjector;
+using Lion.Domain.Models;
+using Lion.DAL.Abstract;
+
+namespace Lion.Business
+{
+    public interface IRepositoryContainer
+    {
+
+        IAddressTypeRepository AddressTypeRepository { get; }
+
+        IBudgetRepository BudgetRepository { get;  }
+
+        ICountryDetailRepository CountryDetailRepository { get;  }
+
+        ICurrencyRepository CurrencyRepository { get;  }
+
+        ILedgerAccountAddressRepository LedgerAccountAddressRepository { get;  }
+
+        ILedgerAccountRepository LedgerAccountRepository { get;  }
+
+        ISecurityQuestionRepository SecurityQuestionRepository { get;  }
+
+        IUserAccountAddressRepository UserAccountAddressRepository { get;  }
+
+        IUserAccountRepository UserAccountRepository { get;  }
+
+        IUserStatusRepositoryRepository UserStatusRepository { get;  }
+
+        IVoucherDetailRepository VoucherDetailRepository { get;  }
+
+        IVoucherHeaderRepository VoucherHeaderRepository { get;  }
+
+        IVoucherTypeRepository VoucherTypeRepository { get;  }
+        
+    }
+}
---
+++ Source/Lion.Business/RepositoryContainer.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lion.DAL.Abstract;
+using Lion.DAL.Concrete;
+using SimpleInjector;
+
+
+
+namespace Lion.Business
+{
+    public class RepositoryContainer : IRepositoryContainer
+    {
+
+        static Container _container;
+
+
+        public RepositoryContainer()
+        {
+
+        }
+
+
+        public RepositoryContainer(Container container)
+        {
+            _container = container;
+        }
+
+        public IAddressTypeRepository AddressTypeRepository
+        {
+            get { return _container.GetInstance<IAddressTypeRepository>(); }
+        }
+
+        public IBudgetRepository BudgetRepository
+        {
+            get { return _container.GetInstance<IBudgetRepository>(); }
+        }
+
+        public ICountryDetailRepository CountryDetailRepository
+        {
+            get { return _container.GetInstance<ICountryDetailRepository>(); }
+        }
+
+        public ICurrencyRepository CurrencyRepository
+        {
+            get { return _container.GetInstance<ICurrencyRepository>(); }
+        }
+
+        public ILedgerAccountAddressRepository LedgerAccountAddressRepository
+        {
+            get { return _container.GetInstance<ILedgerAccountAddressRepository>(); }
+        }
+
+        public ILedgerAccountRepository LedgerAccountRepository
+        {
+            get { return _container.GetInstance<ILedgerAccountRepository>(); }
+        }
+
+        public ISecurityQuestionRepository SecurityQuestionRepository
+        {
+            get { return _container.GetInstance<ISecurityQuestionRepository>(); }
+        }
+
+        public IUserAccountAddressRepository UserAccountAddressRepository
+        {
+            get { return _container.GetInstance<IUserAccountAddressRepository>(); }
+        }
+
+        public IUserAccountRepository UserAccountRepository
+        {
+            get { return _container.GetInstance<IUserAccountRepository>(); }
+        }
+
+        public IUserStatusRepositoryRepository UserStatusRepository
+        {
+            get { return _container.GetInstance<IUserStatusRepositoryRepository>(); }
+        }
+
+        public IVoucherDetailRepository VoucherDetailRepository
+        {
+            get { return _container.GetInstance<IVoucherDetailRepository>(); }
+        }
+
+        public IVoucherHeaderRepository VoucherHeaderRepository
+        {
+            get { return _container.GetInstance<IVoucherHeaderRepository>(); }
+        }
+
+        public IVoucherTypeRepository VoucherTypeRepository
+        {
+            get { return _container.GetInstance<IVoucherTypeRepository>(); }
+        }
+    }
+
+}
